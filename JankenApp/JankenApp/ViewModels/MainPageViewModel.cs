efb16aebca0090ee48816018427bc8fada37526f
ykageyama <ykageyama@stcinc.co.jp>
2020-03-06 13:26:23 +0900

    不要なusingを削除

--- JankenApp/JankenApp/ViewModels/MainPageViewModel.cs
+++ JankenApp/JankenApp/ViewModels/MainPageViewModel.cs
@@ -1,12 +1,6 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using Prism.Navigation;
-using Prism.Services;
+﻿using Prism.Navigation;
 using Reactive.Bindings;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Xamarin.Forms;
